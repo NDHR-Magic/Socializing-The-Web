1393e08619d0eb3cac6aba11dbdcf88b7edc989b
Nathan Peek <nmp14fsu@gmail.com>
2021-03-20 14:43:27 -0400

    Create Songs class for sequelize

---
+++ models/Songs.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection.js");
+
+class Song extends Model { }
+
+Song.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        artist: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        album: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        underscored: true,
+        freezeTableName: true,
+        modelName: "song"
+    }
+)
+
+module.exports = Song;
