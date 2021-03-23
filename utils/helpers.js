3b8d4b1ff23a3269921774fba37c860f4be02986
Nathan Peek <nmp14fsu@gmail.com>
2021-03-22 21:28:38 -0400

    Add helper for allowing editing of head in html in partials/handlebar files that arent the main layout

---
+++ utils/helpers.js
@@ -0,0 +1,7 @@
+module.exports = {
+    section: function (name, options) {
+        if (!this._sections) this._sections = {};
+        this._sections[name] = options.fn(this);
+        return null;
+    }
+}
