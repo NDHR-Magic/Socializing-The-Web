ec68e384cc5b7929511b1227495862ca95e3153c
David Sanchez <davidmsanchez250@gmail.com>
2021-03-23 20:10:01 -0400

    created userRoutes.js, added userRoutes.js to api index file. Created login and signup handlebars files and added them to index.js under viewRoutes.

--- controllers/viewRoutes/index.js
+++ controllers/viewRoutes/index.js
@@ -7,5 +7,17 @@ router.get("/", (req, res) => {
     })
 });
 
+router.get("/login", (req,res)=> {
+    res.render("login", {
+        loggedIn: req.session.loggedIn
+    })
+});
+
+router.get("/signup", (req,res)=> {
+    res.render("signup", {
+        loggedIn: req.session.loggedIn
+    })
+});
+
 
 module.exports = router;
