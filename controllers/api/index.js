f5b56258c88cc2bb7c3908b4812af2c46467cfae
Rachel Diaz <Rachel7113@gmail.com>
2021-03-23 22:52:38 -0400

    Added GET for playlist

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 const songRoutes = require("./songRoutes");
 const userRoutes = require("./userRoutes");
 const noteRoutes = require("./noteRoutes");
+const playlistRoutes = require("./playlistRoutes");
 
 router.use("/songs", songRoutes);
 router.use("/users", userRoutes);
 router.use("/notes", noteRoutes);
+router.use("/playlist", playlistRoutes);
 
 module.exports = router;
