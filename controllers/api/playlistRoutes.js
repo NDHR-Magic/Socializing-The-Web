59e0f603b07bbfe0619615c0874300ec4619a325
Rachel Diaz <Rachel7113@gmail.com>
2021-03-23 21:09:34 -0400

    Added playlist Route & middleware to populate playlist

---
+++ controllers/api/playlistRoutes.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const { Playlist } = require("../../models");
+
+router.post('/', async (req, res) => {
+    try {
+        const newPlaylist = await Playlist.create({
+            ...req.body,
+
+        });
+
+        res.status(200).json(newPlaylist);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
