c5714c58ddc6499f7bb8d0b46241a488957af8fe
Rachel Diaz <Rachel7113@gmail.com>
2021-03-23 21:39:49 -0400

    added export to playlist route & deleted unneccessary middleware

--- controllers/api/playlistRoutes.js
+++ controllers/api/playlistRoutes.js
@@ -5,7 +5,6 @@ router.post('/', async (req, res) => {
     try {
         const newPlaylist = await Playlist.create({
             ...req.body,
-
         });
 
         res.status(200).json(newPlaylist);
@@ -13,3 +12,5 @@ router.post('/', async (req, res) => {
         res.status(400).json(err);
     }
 });
+
+module.exports = router;
