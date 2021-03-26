14c3a299d7bf09d7d378d22419ce09dc494e7f02
Nathan Peek <nmp14fsu@gmail.com>
2021-03-25 22:07:22 -0400

    Merger hader files

--- controllers/api/noteRoutes.js
+++ controllers/api/noteRoutes.js
@@ -2,18 +2,66 @@ const router = require("express").Router();
 const { Song, Tag, Notes, User } = require("../../models");
 
 
-router.get("/",async(req,res)=>{
+router.get("/", async (req, res) => {
     const noteResults = await Notes.findAll({
-        include:{
+        include: {
             model: User
         }
     });
     console.log(noteResults);
 
-    const notes = noteResults.map(note => note.get({plain:true}))
+    const notes = noteResults.map(note => note.get({ plain: true }))
     res.json(notes);
 })
 
+
+
+router.post('/', async (req, res) => {
+    try {
+        const noteData = await Notes.create({
+            // id: req.body.id,
+            title: req.body.title,
+            // artist: req.body.artist,
+            // album: req.body.album,
+            description: req.body.description,
+            // playlist_id: req.body.playlist_id,
+            user_id: req.session.user_id,
+            // song_id: req.body.song_id,
+            // tag_id: req.body.tag_id,
+
+        });
+        res.status(200).json(noteData)
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.put('/:id', async (req, res) => {
+
+    try {
+        const noteX = await Notes.update(
+            {
+                id: req.body.id,
+                title: req.body.title,
+                artist: req.body.artist,
+                album: req.body.album,
+                description: req.bodydescription,
+                playlist_id: req.body.playlist_id,
+                user_id: req.body.user_id,
+                song_id: req.body.song_id,
+                tag_id: req.body.tag_id,
+            },
+            {
+                where: {
+                    id: req.params.id,
+                },
+            });
+
+        res.status(200).json(noteX);
+    } catch (err) {
+        res.status().json(err);
+    };
+});
 
 
 module.exports = router;
