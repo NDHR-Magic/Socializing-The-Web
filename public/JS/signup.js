df637e150d4de45f1748ff3addb01662b6cf892f
David Sanchez <davidmsanchez250@gmail.com>
2021-03-25 21:23:32 -0400

    edited signup view to include first name, last name, username, email, password. Edited front end JS to talk to back end to create user

--- public/JS/signup.js
+++ public/JS/signup.js
@@ -1,3 +1,39 @@
-// add event listeners
+$(document).ready(function() {
 
-//fetch calls
+    const signupButton = $('#signupButton');
+    
+    signupButton.on('click', async function(){
+        const first_name = $('#firstName').val();
+        const last_name = $('#lastName').val();
+        const username = $('#username').val();
+        const password = $('#password').val();
+        const email = $('#email').val();
+        
+    
+        if (first_name && last_name && username && email && password) {
+            console.log('if else start')
+            //fetch call
+            const response = await fetch('api/users/signup', {
+                method: 'POST',
+                body: JSON.stringify({first_name, last_name, username, email, password}),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            
+            });
+    
+        if (response.ok) {
+            document.location.replace("/member")
+        } else {
+            console.log("response is not ok");
+        }
+    
+        } else {
+            console.log("error")
+            //change later
+        }
+    
+    })
+    
+    
+    })
