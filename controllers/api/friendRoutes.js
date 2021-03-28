aed62857abf237b47e86250f541593632089f2fe
Nathan Peek <nmp14fsu@gmail.com>
2021-03-28 02:38:09 -0400

    Remove test friend route

--- controllers/api/friendRoutes.js
+++ controllers/api/friendRoutes.js
@@ -7,25 +7,6 @@ router.param("userId", async (req, res, next, id) => {
     next();
 });
 
-router.get("/", async (req, res) => {
-    try {
-        const user = await User.findByPk(1);
-        const user2 = await User.findByPk(2);
-        const user3 = await User.findByPk(3);
-        const user4 = await User.findByPk(4);
-        const user5 = await User.findByPk(5);
-
-        await user.addFriend([user2, user4]);
-        await user3.addFriend([user5, user2]);
-
-        const userFriends = await user.getFriend();
-
-        res.json(userFriends);
-    } catch (e) {
-        res.status(500).json(e);
-    }
-});
-
 router.post("/request/:userId", async (req, res) => {
     try {
         const requester = await User.findByPk(5);
