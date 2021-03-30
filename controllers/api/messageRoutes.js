a6e3fb3de8d26e5e2f672e97471d87c02eeb6b7c
Nathan Peek <nmp14fsu@gmail.com>
2021-03-30 18:38:43 -0400

    Create route for linking messengers/receivers

--- controllers/api/messageRoutes.js
+++ controllers/api/messageRoutes.js
@@ -1,4 +1,33 @@
+const { User } = require("../../models");
+
 const router = require("express").Router();
 
+router.post("/sendMessage/:id", async (req, res) => {
+    try {
+        const currentUser = await User.findByPk(req.session.user_id);
+        const userToMessage = await User.findByPk(req.params.id);
+
+        // see if both users exist
+        if (currentUser && userToMessage) {
+            // check if they have a chat history
+            const checkMessage = await currentUser.hasReceiver(userToMessage);
+
+            console.log(checkMessage);
+
+            if (!checkMessage) {
+                await currentUser.addReceiver(userToMessage);
+                res.status(201).json({ message: "Created message" });
+            } else {
+                res.status(302).json({ message: "Messages found" });
+            }
+        } else {
+            res.status(404).json({ message: "User not found" });
+            return;
+        }
+
+    } catch (e) {
+        res.status(500).json(e);
+    }
+});
 
 module.exports = router;
