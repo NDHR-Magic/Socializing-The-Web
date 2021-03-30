5ed376c626583f1d494c75aeaaff4c3d239356e7
Nathan Peek <nmp14fsu@gmail.com>
2021-03-30 16:16:56 -0400

    Add notes to userProfiles with style

---
+++ public/JS/notes.js
@@ -0,0 +1,34 @@
+const noteSubmitBtn = document.getElementById("noteSubmit");
+
+const createNote = async (e) => {
+    e.preventDefault();
+
+    const title = document.getElementById("noteTitle").value.trim();
+    const description = document.getElementById("noteDescription").value.trim();
+
+    console.log("test");
+
+    if (title && description) {
+        const noteResponse = await fetch("/api/notes/", {
+            method: "POST",
+            body: JSON.stringify({ title, description }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (noteResponse.ok) {
+            window.location.replace("/member");
+        } else {
+
+        }
+    } else {
+        const p = document.createElement("p");
+        p.textContent = "Fill out title and description."
+        p.setAttribute("class", "red has-text-centered ml-5");
+
+        document.getElementById("form").appendChild(p);
+    }
+}
+
+if (noteSubmitBtn) {
+    noteSubmitBtn.addEventListener("click", createNote);
+}
