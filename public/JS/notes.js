af6d4cd5cb339ec730b1bf63f9fa6adf0ee166da
Nathan Peek <nmp14fsu@gmail.com>
2021-03-31 16:20:23 -0400

    Create fetch requests to link song to note creation

--- public/JS/notes.js
+++ public/JS/notes.js
@@ -1,13 +1,17 @@
 const noteSubmitBtn = document.getElementById("noteSubmit");
 const deleteNoteBtn = document.querySelectorAll("noteDelete");
+const linkSongBtn = document.getElementById("linkSong");
+const submitSongBtn = document.getElementById("submitSong");
+const rejectLinkBtn = document.getElementById("rejectLink");
+
+// New note's id
+let noteId;
 
 const createNote = async (e) => {
     e.preventDefault();
 
     const title = document.getElementById("noteTitle").value.trim();
     const description = document.getElementById("noteDescription").value.trim();
-
-    console.log("test");
 
     if (title && description) {
         const noteResponse = await fetch("/api/notes/", {
@@ -16,8 +20,13 @@ const createNote = async (e) => {
             headers: { 'Content-Type': 'application/json' }
         });
 
+        const noteJSON = await noteResponse.json();
+
         if (noteResponse.ok) {
-            window.location.replace("/member");
+            noteId = noteJSON.id;
+            document.getElementById("form").setAttribute("class", "hidden");
+            document.getElementById("songForm").classList.remove("hidden");
+
         } else {
 
         }
@@ -30,8 +39,44 @@ const createNote = async (e) => {
     }
 }
 
-if (noteSubmitBtn) {
-    noteSubmitBtn.addEventListener("click", createNote);
-};
+// function to link song to note (Causes form to show on page)
+const linkSong = (e) => {
+    e.preventDefault();
 
+    document.getElementById("songSearchForm").classList.remove("hidden");
+}
 
+// Actual function to do the linking of song
+const submitSong = async (e) => {
+    e.preventDefault();
+
+    const songName = document.getElementById("songInput").value.trim();
+
+    const linkSongResponse = await fetch("/api/notes/addSong", {
+        method: "POST",
+        body: JSON.stringify({ songName, noteId }),
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (linkSongResponse.status === 200) {
+        document.location.replace("/userProfile");
+    } else {
+        const p = document.createElement("p");
+        p.textContent = "Could not find song"
+        p.setAttribute("class", "red has-text-centered ml-5");
+
+        document.getElementById("songForm").appendChild(p);
+    }
+}
+
+const rejectSong = (e) => {
+    e.preventDefault();
+
+    document.location.replace("/member");
+}
+
+// Add event listeners to buttons if they exist
+if (noteSubmitBtn) noteSubmitBtn.addEventListener("click", createNote);
+if (linkSongBtn) linkSongBtn.addEventListener("click", linkSong);
+if (submitSongBtn) submitSongBtn.addEventListener("click", submitSong);
+if (rejectLinkBtn) rejectLinkBtn.addEventListener("click", rejectSong);
