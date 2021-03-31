bdbe4f4a76713161e8de1b2f264f8541deb9a58b
Nathan Peek <nmp14fsu@gmail.com>
2021-03-30 21:43:47 -0400

    Add fetch request to add song to selected playlist

--- public/JS/addSongs.js
+++ public/JS/addSongs.js
@@ -1,39 +1,79 @@
 const addSongBtn = document.querySelector('#addtoPlaylist');
+let addToPlaylistBtns;
+let songId;
 
-const addSongs = async (e) => {
-    addSongBtn.addEventListener('click', async function (e) {
-        e.preventDefault()
-        console.log('I have been clicked')
-        addSongBtn.classList.add('hidden')
+const addSong = async (e) => {
+    e.preventDefault()
+    console.log('I have been clicked')
+    addSongBtn.classList.add('hidden')
 
-        const ID = parseInt(e.target.getAttribute("data-id"));
-        const getPlaylist = await fetch(`/api/playlist/`, {
-            method: "GET"
-        })
+    const ID = parseInt(e.target.getAttribute("data-id"));
+    songId = ID;
 
-        if (getPlaylist.status === 200) {
-            const playlistData = await getPlaylist.json()
-            console.log(playlistData)
-
-            for (const playlist of playlistData) {
-                createlistItems(playlist)
-            }
-        }
-        // const addSongs = await fetch(`/api/songs/addtoPlaylist/${ID}`, {
-        //     method: "POST"
-        // });
+    const getPlaylist = await fetch(`/api/playlist/`, {
+        method: "GET"
     })
+
+    if (getPlaylist.status === 200) {
+        const playlistData = await getPlaylist.json()
+        console.log(playlistData)
+
+        for (const playlist of playlistData) {
+            createlistItems(playlist)
+        }
+    }
+
+    addToPlaylistBtns = document.querySelectorAll(".addToList");
+    if (addToPlaylistBtns) {
+        for (const btn of addToPlaylistBtns) {
+            btn.addEventListener("click", selectPlayList);
+        }
+    }
+}
+
+const selectPlayList = async (e) => {
+    e.preventDefault();
+
+    const playlistID = parseInt(e.target.getAttribute("data-playlist"));
+
+    const addResponse = await fetch(`/api/songs/addtoPlaylist/${songId}`, {
+        method: "POST",
+        body: JSON.stringify({ playlistID }),
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (addResponse.status === 200) {
+        const ul = document.querySelector('#playlists');
+
+        const p = document.createElement("p");
+        p.setAttribute("class", "green");
+        p.textContent = "Song added to playlist"
+
+        ul.append(p);
+    } else if (addResponse.status === 304) {
+        const ul = document.querySelector('#playlists');
+
+        const p = document.createElement("p");
+        p.setAttribute("class", "red");
+        p.textContent = "Song already in playlist"
+
+        ul.append(p);
+    } else {
+        console.log("bad");
+    }
 }
 
 function createlistItems(playlist) {
     const ul = document.querySelector('#playlists');
 
     const li = document.createElement('li')
-    const btn = document.createElement('button')
+    const btn = document.createElement('button');
+    btn.setAttribute("class", "addToList");
+    btn.setAttribute("data-playlist", playlist.id);
 
     btn.textContent = playlist.name
     li.appendChild(btn)
     ul.appendChild(li)
 }
 
-addSongs();
+addSongBtn.addEventListener('click', addSong);
