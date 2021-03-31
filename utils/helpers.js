67c9b2106258b22859ed1dd1adad7aceef365148
Nathan Peek <nmp14fsu@gmail.com>
2021-03-30 20:35:46 -0400

    Add forrmat for time and dates

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,5 +3,11 @@ module.exports = {
         if (!this._sections) this._sections = {};
         this._sections[name] = options.fn(this);
         return null;
-    }
+    },
+    format_time: (date) => {
+        return date.toLocaleTimeString();
+    },
+    format_date: (date) => {
+        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
+    },
 }
