0b11b1263d96a58d94125a003a94dbfc3f74b3c7
Nathan Peek <nmp14fsu@gmail.com>
2021-04-01 16:50:08 -0400

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(
         process.env.DB_NAME,
         process.env.DB_USER,
-        process.env.DB_PW,
+        process.env.DB_PASSWORD,
         {
             host: 'localhost',
             dialect: 'mysql',
@@ -14,5 +14,3 @@ if (process.env.JAWSDB_URL) {
         },
     );
 }
-
-module.exports = sequelize;
