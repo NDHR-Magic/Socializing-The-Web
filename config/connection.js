318a4aec2c39f9a8df086c264ad6b826f049ebe0
Nathan Peek <nmp14fsu@gmail.com>
2021-04-01 16:53:31 -0400

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,6 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
@@ -14,3 +17,5 @@ if (process.env.JAWSDB_URL) {
         },
     );
 }
+
+module.exports = sequelize
